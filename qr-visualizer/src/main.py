ee5b97217b91769833fdb13a5d82e9cbcae8715e
Juan Mosquera <jdmm360@gmail.com>
2023-11-09 14:47:00 -0500

    fix: remove types to use in raspberry

--- qr-visualizer/src/main.py
+++ qr-visualizer/src/main.py
@@ -1,7 +1,6 @@
 from modules.inventory.adapters.products_repository import ProductsRepository
 from modules.inventory.application.products_service import ProductsService
 from modules.drone.application.drone_scanner import DroneScanner
-from modules.camera.infrastructure.adapters.ocv_camera import Camera
 
 repo = ProductsRepository()
 products_service = ProductsService(repo)
